9ca9398afd9cb7327452f3a11d4e068cf2ee7832
kjartan89 <kjartan_melbye@hotmail.com>
2025-04-08 11:27:03 +0200

    glr til lrs

--- modul8github/main.tf
+++ modul8github/main.tf
@@ -26,6 +26,6 @@ resource "azurerm_storage_account" "sakm" {
   resource_group_name      = azurerm_resource_group.rg.name  
   location                 = azurerm_resource_group.rg.location  
   account_tier             = "Standard"
-  account_replication_type  = "GRS"
+  account_replication_type  = "LRS"
 }
 
